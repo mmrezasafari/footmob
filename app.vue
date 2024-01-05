2f36ae0e92a7875617116e9f4b553578dfcec915
mmreza <mohammadrezasafari23@gmail.com>
2024-01-05 22:39:34 +0330

    added :: added pinia store to project

--- app.vue
+++ app.vue
@@ -1,4 +1,15 @@
 <template>
   <div>
+    <section>
+
+      {{ counter }}
+    </section>
   </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const counter = ref(0)
+
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,6 @@ export default defineNuxtConfig({
 
   modules: [
     'nuxt-quasar-ui',
+    '@pinia/nuxt'
   ],
 })
