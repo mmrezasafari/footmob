de1212d99e142ee0802f81f0723b30a25a54a0e2
mmreza <mohammadrezasafari23@gmail.com>
2024-01-15 00:23:47 +0330

    modify :: modified indent by formater

--- app.vue
+++ app.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 const colorMode = useColorMode()
 
-const color = computed(() => colorMode.value === 'dark' ?  '#111827' : 'white')
+const color = computed(() => colorMode.value === 'dark' ? '#111827' : 'white')
 
 useHead({
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-    { key: 'theme-color', name: 'theme-color', content: color}
+    { key: 'theme-color', name: 'theme-color', content: color }
   ],
   htmlAttrs: {
     lang: 'fa',
@@ -25,7 +25,7 @@ useSeoMeta({
     <NuxtLoadingIndicator />
 
     <NuxtLayout>
-      <NuxtPage/>
+      <NuxtPage />
     </NuxtLayout>
 
   </div>
