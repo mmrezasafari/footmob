2d96e0cf5b6689b6127dea698bbbd38518346bf0
mmreza <mohammadrezasafari23@gmail.com>
2024-01-15 00:23:17 +0330

    Create :: created a layout components
    created FooterComponent
    created NavBarComponent
    modified default layout and added layout components

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,31 +1,15 @@
 <template>
-    <q-layout view="hHh lpR fFf">
+  <q-layout view="hHh lpR fFf">
+    <NavBar />
 
-        <q-header elevated class="bg-primary text-white">
-            <q-toolbar>
-                <q-toolbar-title>
-                    <q-avatar>
-                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-                    </q-avatar>
-                    Title
-                </q-toolbar-title>
-            </q-toolbar>
-        </q-header>
+    <q-page-container>
+      <slot />
+    </q-page-container>
 
-        <q-page-container>
-            <slot />
-        </q-page-container>
-
-        <q-footer elevated class="bg-grey-8 text-white">
-            <q-toolbar>
-                <q-toolbar-title>
-                    <q-avatar>
-                        <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
-                    </q-avatar>
-                    <div>Title</div>
-                </q-toolbar-title>
-            </q-toolbar>
-        </q-footer>
-
-    </q-layout>
+    <Footer />
+  </q-layout>
 </template>
+<script setup lang="ts">
+import NavBar from '~/components/layouts/NavBar.vue'
+import Footer from '~/components/layouts/Footer.vue'
+</script>
