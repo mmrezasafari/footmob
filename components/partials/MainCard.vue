c2345324e657170a44ed1a78bf041f93ba498e16
mmrezasafari <mmrezadev@gmail.com>
2024-02-19 01:12:48 +0330

    create :: created tow Cards components for main content and aside content

---
+++ components/partials/MainCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card class="mx-auto" rounded="lg">
+    <v-card-item class="bg-blue-grey-lighten-4">
+      <v-card-title>
+        <slot name="title" />
+      </v-card-title>
+      <v-card-subtitle>
+        <slot name="subtitle" />
+      </v-card-subtitle>
+    </v-card-item>
+    <v-card-text class="pa-5 bg-blue-grey-lighten-5">
+      <slot name="content" />
+    </v-card-text>
+  </v-card>
+</template>
---
+++ components/partials/SideCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-card class="mx-auto bg-blue-grey-lighten-5" rounded="lg">
+    <v-card-item class="border-b">
+      <v-card-title>
+        <slot name="title" />
+      </v-card-title>
+      <v-card-subtitle>
+        <slot name="subtitle" />
+      </v-card-subtitle>
+    </v-card-item>
+    <v-card-text class="pa-5">
+      <slot name="content" />
+    </v-card-text>
+  </v-card>
+</template>
