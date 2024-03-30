fbf7de05ac09da8a24ef7aca5797fe5e79f8006d
mmrezasafari <mmrezadev@gmail.com>
2024-03-30 19:09:01 +0330

    add :: added popular,countries and international leagues computed

--- store/leaguse.ts
+++ store/leaguse.ts
@@ -1,16 +1,26 @@
-import { defineStore } from 'pinia'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+import type { AllLeagues } from "fotmob/dist/esm/types/all-leagues";
 
-export const useLeaguesStore = defineStore('leaguesStore', () => {
-    const leagues = ref(null)
+export const useLeaguesStore = defineStore("leaguesStore", () => {
+  const allLeagues = ref<AllLeagues>();
 
-    async function getAllLeagues() {
-        try {
-            const { data } = await $fetch('/backEnd/allLeagues')
-            leagues.value = data
-        } catch (err) {
-            console.error(err);
-        }
+  const popularLeagues = computed(() => allLeagues.value?.popular)
+  const countries = computed(() => allLeagues.value?.countries)
+  const internationalLeagues = computed(() => allLeagues.value?.international)
+
+  async function fetchAllLeagues() {
+    try {
+      const data: AllLeagues = await $fetch("/backEnd/allLeagues");
+      if (data) {
+        allLeagues.value = data
+      }
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error("fetchAllLeagues default error", err.message);
+      }
     }
+  }
 
-    return { getAllLeagues, leagues }
-})
+  return { fetchAllLeagues, allLeagues, popularLeagues, internationalLeagues, countries };
+});
