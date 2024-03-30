39a172e4768df120967d3288a44a3672a420f05e
mmrezasafari <mmrezadev@gmail.com>
2024-03-30 19:06:51 +0330

    modify :: modified margin and spacing

--- components/partials/SideCard.vue
+++ components/partials/SideCard.vue
@@ -1,14 +1,14 @@
 <template>
   <v-card class="mx-auto bg-blue-grey-lighten-5" rounded="lg">
-    <v-card-item class="border-b">
+    <v-card-item class="border-b px-3 py-1">
       <v-card-title>
         <slot name="title" />
       </v-card-title>
-      <v-card-subtitle>
+      <v-card-subtitle v-if="$slots.subtitle">
         <slot name="subtitle" />
       </v-card-subtitle>
     </v-card-item>
-    <v-card-text class="pa-5">
+    <v-card-text class="pa-0">
       <slot name="content" />
     </v-card-text>
   </v-card>
